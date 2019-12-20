d4bb8c69f681c7cf274a81e8f2aa4aeaf13eaca9
Jimi <jimitogni@gmail.com>
2019-12-20 13:48:13 -0300

    ferias

--- jogo/jogo.py
+++ jogo/jogo.py
@@ -21,6 +21,7 @@ char = pygame.image.load('boneco.png')
 win = pygame.display.set_mode((900, 600))
 pygame.display.set_caption("Darwin Algoritimo Evolutivo")
 
+score = 0
 
 class player(object):
     def __init__(self, x, y, width, height):
@@ -34,6 +35,7 @@ class player(object):
         self.left = False
         self.right = False
         self.walk_count = 0
+        self.box = (self.x + 10, self.y, 100, 150)
 
     def draw(self, win):
         if self.walk_count + 1 >= 27:
@@ -47,20 +49,71 @@ class player(object):
             self.walk_count += 1
         else:
             win.blit(char, (self.x, self.y))
+        self.box = (self.x + 10, self.y, 100, 150)
+        pygame.draw.rect(win, (255,0,0), self.box, 2)
 
 
 class enemy(object):
     walk_left = [pygame.image.load('gato.png'), pygame.image.load('dog.png'), pygame.image.load('gir.png'), pygame.image.load('rato.png')]
 
+    def __init__(self, x, y, width, height, end):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.path = [self.x, self.y]
+        self.end = end
+        self.walk_count = 0
+        self.vel = 10
+        self.box = (self.x, self.y, 80, 100)
+
+    def draw(self, win):
+        self.move()
+        if self.walk_count + 1 <= 33:
+            self.walk_count = 0
+
+        if self.vel > 0:
+            win.blit(self.walk_left[self.walk_count//3], (self.x, self.y))
+            self.walk_count += 1
+        else:
+            win.blit(self.walk_left[self.walk_count//3], (self.x, self.y))
+            self.walk_count += 1
+        self.box = (self.x, self.y, 80, 100)
+        pygame.draw.rect(win, (255,0,0), self.box, 2)
+
+
+    def move(self):
+        if self.x > 10*-1:
+        #    if self.x + self.vel < self.path[0]:
+            self.x -= self.vel
+        else:
+            self.x = 900
+            #self.x -= self.vel
+            #        self.vel = self.vel * -1
+        #        self.walk_count = 0
+        #else:
+        #    if self.x - self.vel > self.path[1]:
+        #        self.x += self.vel
+        #    else:
+        #        self.vel = self.vel * -1
+        #        self.walk_count = 0
+
+
 
 def redraw_game_window():
     win.blit(bg, (0, 0))  # back ground image
+    text = font.render('Pontuacao: ' + str(score), 1, (0,0,0))
+    win.blit(text, (10, 30))
     man.draw(win)
+    gobling.draw(win)
     pygame.display.update()
 
 
-man = player(300, 400, 64, 64)
+# main looping
+man = player(10, 350, 64, 64)
+gobling = enemy(900, 390, 64, 64, 300)
 run = True
+font = pygame.font.SysFont('comicsans', 50, True)
 
 while run:
 
@@ -101,6 +154,19 @@ while run:
         else:
             man.is_jump = False
             man.jump_count = 10
+
+    # colision
+    if man.box[2] >= gobling.box[3]+10:
+        print('parametros PULANDO')
+        print(man.box[2])
+        print(gobling.box[3])
+        print('\n')
+        score += 1
+    elif man.box[2] == gobling.box[3]:
+        print('parametros dos objetos SEM PULAR')
+        print(man.box[2])
+        print(gobling.box[3])
+        print('\n')
 
     redraw_game_window()
 
